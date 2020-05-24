a9c5573d582937ded5792745ce8b9b5e7bb0925c
Jared Berger <jaredberger@Jareds-MBP.fios-router.home>
2020-05-24 15:09:24 -0400

    updated usage info

--- lang/project/ProjectInterpreter.fs
+++ lang/project/ProjectInterpreter.fs
@@ -7,8 +7,11 @@ type Env = Map<string, TuneBuilder>
 
 // function to provide usage info
 let usage = 
-    printfn "\nUsage:\ndotnet run <program.arp> <output.wav>\nor\ndotnet run <program.arp> <output.wav> <beats per minute>\n"
-    printfn "To run the program \'example-1.arp\' in the \'examples\' directory at 150 BPM type:\ndotnet run ../examples/example-1.arp example-1.wav 150\n"
+    printfn "\nUsage:\n\ndotnet run <program.arp> <output.wav>\nor\ndotnet run <program.arp> <output.wav> <beats per minute>,"
+    printfn "\nwhere <program.arp> is a path to an Arpeggify program\nand <output.wav> is a string representating the name of the output file\n"
+    printfn "Example: From within 'project' directory type:"
+    printfn "dotnet run ../../examples/example-1.arp example-1.wav 200\n"
+
     printfn "Arpeggify arpeggiates the tune provided in the last line of a program, \nwriting the musical output to a wav file"
     printfn "\n## END ARPEGGIFY INTERPRETER ##"
 
